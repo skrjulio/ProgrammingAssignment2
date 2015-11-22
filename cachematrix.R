4ba334d479e968e5f1157a5e3159117d98472060
Ms. S. Julio <shelleyjulio@JEW-Pro-3.local>
2015-11-21 22:53:45 -0800

    working code, add comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix creates object that can cache its inverse
 
 makeCacheMatrix <- function(X = matrix()) {
-    get <- function() X
-    setinverse <- function(inv) {
-        I <<- inv
+    I <- NULL
+    # set is a function that changes the matrix X stored in the main function
+    set <- function (Y) {
+        X <<- Y
+        I <<- NULL
     }
+    # get is a function that returns the matrix X in the main function
+    get <- function() X
+    # setinverse stores the value of the input from line 29 into the variable I
+    setinverse <- function(inv) I <<- inv
+    # getinverse returns the value I
     getinverse <- function() I
-    list(get = get, setinverse = setinverse, getinverse = getinverse)
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
-## Write a short comment describing this function
-## cacheSolve is a function that calculates (or re-calculates) the inverse of an invertible matrix
+## cacheSolve(x) is a function that calculates the inverse of an invertible matrix, but first checks to see if the inverse has already been calculated
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
         I <- x$getinverse()
+        # checks to see if there is a matrix stored with getinverse (not NULL), if it exits a message and value I is returned
         if(!is.null(I)) {
             message("getting cached data")
-            print(I)
             return(I)
         }
         data <- x$get()
+        ## solve returns a matrix that is the inverse of 'x'
         I <- solve(data, ...)
+        # stores the matrix I it the object assigned with makeCacheMatrix
         x$setinverse(I)
         I
-        print(I)
 }
